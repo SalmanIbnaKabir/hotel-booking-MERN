496c35bf9e339c2304b92e03acc8e277ff4aa14f
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2023-12-31 00:26:04 +0600

    Payment problem solved

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -70,27 +70,29 @@ const Search = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
-        <div className="space-y-5">
-          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+      <div className="rounded-lg border border-slate-300 p-5  h-fit lg:sticky top-10">
+        <div className="">
+          <h3 className="text-lg font-semibold border-b border-separate border-slate-300 pb-5">
             Filter by:
           </h3>
-          <StarRatingFilter
-            selectedStars={selectedStars}
-            onChange={handleStarsChange}
-          />
-          <HotelTypesFilter
-            selectedHotelTypes={selectedHotelTypes}
-            onChange={handleHotelTypeChange}
-          />
-          <FacilitiesFilter
-            selectedFacilities={selectedFacilities}
-            onChange={handleFacilityChange}
-          />
-          <PriceFilter
-            selectedPrice={selectedPrice}
-            onChange={(value?: number) => setSelectedPrice(value)}
-          />
+          <div className="grid grid-cols-2 md:grid-cols-4 justify-between lg:grid-cols-1">
+            <StarRatingFilter
+              selectedStars={selectedStars}
+              onChange={handleStarsChange}
+            />
+            <HotelTypesFilter
+              selectedHotelTypes={selectedHotelTypes}
+              onChange={handleHotelTypeChange}
+            />
+            <FacilitiesFilter
+              selectedFacilities={selectedFacilities}
+              onChange={handleFacilityChange}
+            />
+            <PriceFilter
+              selectedPrice={selectedPrice}
+              onChange={(value?: number) => setSelectedPrice(value)}
+            />
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-5">
